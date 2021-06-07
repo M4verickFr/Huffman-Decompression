95b759a6b752b1968a967fc4420172c8dd0e4355
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-06-07 21:58:05 +0200

    display tree in psvm

--- src/main/java/HuffmanDecode.java
+++ src/main/java/HuffmanDecode.java
@@ -4,23 +4,31 @@ import java.util.Dictionary;
 
 public class HuffmanDecode {
 
+    HuffmanTree tree;
     String result = "";
 
     public HuffmanDecode(String frequencyFilePath, String binaryFilePath) {
+        // task3_read_frequencies
         HuffmanFile freqFile = new HuffmanFile(frequencyFilePath);
         Dictionary frequency = freqFile.readFreq();
 
-        HuffmanTree tree = new HuffmanTree(frequency);
+        // task4_generate_huffman_tree
+        this.tree = new HuffmanTree(frequency);
 
+        // task5_read_binaries
         HuffmanFile binaryFile = new HuffmanFile(binaryFilePath);
         String binaryString = binaryFile.readBin();
 
-        this.result = tree.decode(binaryString);
+        // task6_decode_binaries
+        this.result = this.tree.decode(binaryString);
 
+        // task7_write_result
         String outputFilePath = HuffmanFile.getOutputFilePath(freqFile);
         HuffmanFile outputFile = new HuffmanFile(outputFilePath);
         outputFile.writeResult(this.result);
     }
+
+    public HuffmanTree getTree() { return tree; }
 
     public String getResult() {
         return result;
@@ -29,5 +37,6 @@ public class HuffmanDecode {
     public static void main(String[] args) {
         HuffmanDecode huffmanDecode = new HuffmanDecode(args[0], args[1]);
         System.out.println(huffmanDecode.getResult());
+        System.out.println(huffmanDecode.getTree());
     }
 }
