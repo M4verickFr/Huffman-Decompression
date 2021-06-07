4794c45b666384729707714ee318e90b5ef158cd
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-06-07 16:09:05 +0200

    add HuffmanDecode class and declare the methods

---
+++ src/main/java/HuffmanDecode.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class HuffmanDecode {
+
+    public HuffmanDecode(String frequencyFilePath, String binaryFilePath) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+        new HuffmanDecode(args[0], args[1]);
+    }
+}
