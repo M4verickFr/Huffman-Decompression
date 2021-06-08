b94ad0d6d24ce0303d0f217cb85d5a7994bbd3eb
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-06-08 10:15:26 +0200

    comment HuffmanFile class

--- src/main/java/HuffmanFile.java
+++ src/main/java/HuffmanFile.java
@@ -10,21 +10,64 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Scanner;
 
+/**
+ * <b>HuffmanFile est la classe représentant un fichier Huffman.</b>
+ * <p>
+ * Un fichier Huffman est caractérisé par un fichier.
+ * </p>
+ *
+ * @author M4verickFr
+ */
 public class HuffmanFile {
+    /**
+     * Le fichier correspond au fichier Huffman.
+     *
+     * @see HuffmanFile#getName()
+     * @see HuffmanFile#getPath()
+     */
     private File file;
 
+    /**
+     * Constructeur HuffmanFile
+     * <p>
+     *     Le fichier "file" correspond au fichier du "path"
+     * </p>
+     *
+     * @param path
+     *              Chemin d'accès au fichier.
+     *
+     * @see HuffmanFile#file
+     */
     public HuffmanFile(String path) {
         this.file = new File(path);
     }
 
+    /**
+     * Retourne le chemin absolue du fichier.
+     *
+     * @return le chemin du fichier.
+     */
     public String getPath() {
         return file.getAbsolutePath();
     }
 
+    /**
+     * Retourne le nom du fichier.
+     *
+     * @return le nom du fichier
+     */
     public String getName() {
         return this.file.getName();
     }
 
+    /**
+     * Lecture d'un fichier frequence.
+     * <p>
+     * Retourne la liste des frequences presente dans le fichier.
+     * </p>
+     *
+     * @return un dictionnary avec les frequences.
+     */
     public Dictionary readFreq() {
         try {
             Dictionary frequency = new Hashtable();
@@ -43,6 +86,14 @@ public class HuffmanFile {
         }
     }
 
+    /**
+     * Lecture d'un fichier binaire.
+     * <p>
+     * Retourne la chaine binaire encoder dans le fichier.
+     * </p>
+     *
+     * @return la chaine binaire.
+     */
     public String readBin() {
         try {
             String binString = "";
@@ -58,6 +109,15 @@ public class HuffmanFile {
         }
     }
 
+    /**
+     * Ecriture du fichier result.
+     * <p>
+     * Créer et écrit dans le fichier Huffman la chaine "result".
+     * </p>
+     *
+     * @param result
+     *              Chaine a écrire dans le fichier result.
+     */
     public void writeResult(String result) {
         try {
             FileWriter fileWriter = new FileWriter(this.file);
@@ -68,6 +128,17 @@ public class HuffmanFile {
         }
     }
 
+    /**
+     * Genere le chemin du fichier résultat.
+     * <p>
+     * Créer le chemin du fichier r"sultat a l'aide du chemin du fichier passé en paramètre.
+     * </p>
+     *
+     * @param file
+     *              Fichier situé dans le dossier où écrit le résultat.
+     *
+     * @return le chemin du fichier résultat.
+     */
     public static String getOutputFilePath(HuffmanFile file) {
         Path path = Paths.get(file.getPath());
         String outputFilePath = path.getParent() + "/";
