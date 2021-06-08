236bcf84c239a17b22e3f87050e9d0562d99f231
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-06-08 10:24:51 +0200

    comment HuffmanDecode class

--- src/main/java/HuffmanDecode.java
+++ src/main/java/HuffmanDecode.java
@@ -2,11 +2,45 @@ package main.java;
 
 import java.util.Dictionary;
 
+/**
+ * <b>HuffmanDecode est la classe principale du programme.</b>
+ * <p>
+ * Cette classe est caractérisée par les informations suivantes :
+ * <ul>
+ * <li>Un HuffmanTree correspondant à l'arbre de Huffman.</li>
+ * <li>Une chaine de caractères result correspondant au décodage de la chaine binaire.</li>
+ * </ul>
+ * </p>
+ *
+ * @author M4verickFr
+ */
 public class HuffmanDecode {
-
+    /**
+     * L'arbre Huffman permettant de decoder la chaine binaire.
+     *
+     * @see HuffmanDecode#getTree()
+     */
     HuffmanTree tree;
+
+    /**
+     * Le texte decoder a l'aide de l'arbre de Huffman.
+     *
+     * @see HuffmanDecode#getResult()
+     */
     String result = "";
 
+    /**
+     * Constructeur HuffmanDecode.
+     * <p>
+     * A la construction d'un objet HuffmanDecode, la chaine "result" correspond à la chaine binaire decoder.
+     * </p>
+     *
+     * @param frequencyFilePath
+     *                      Chemin du fichier contenant les fréquences.
+     * @param binaryFilePath
+     *                      Chemin du fichier contenant la chaine binaire.
+     *
+     */
     public HuffmanDecode(String frequencyFilePath, String binaryFilePath) {
         // task3_read_frequencies
         HuffmanFile freqFile = new HuffmanFile(frequencyFilePath);
@@ -28,12 +62,26 @@ public class HuffmanDecode {
         outputFile.writeResult(this.result);
     }
 
+    /**
+     * Retourne l'arbre de Huffman permettant de decoder la chaine binaire.
+     *
+     * @return l'arbre binaire.
+     */
     public HuffmanTree getTree() { return tree; }
 
+    /**
+     * Retourne la chaine binaire decoder a l'aide de l'arbre de Huffman.
+     *
+     * @return la chaine binaire.
+     */
     public String getResult() {
         return result;
     }
 
+    /**
+     * Créer l'objet HuffmanDecode.
+     * Affichage le résultat, et l'arbre de Huffman généré.
+     */
     public static void main(String[] args) {
         HuffmanDecode huffmanDecode = new HuffmanDecode(args[0], args[1]);
         System.out.println(huffmanDecode.getResult());
